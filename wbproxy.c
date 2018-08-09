0667238eab3fd30fb166fe546e1b2df3051dd935
huangweibin <huangweibin@kuaishou.com>
2018-08-09 21:16:16 +0800

    log SO_SNDBUF SO_RCVBUF

--- wbproxy.c
+++ wbproxy.c
@@ -432,9 +432,13 @@ void start() {
     if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt)) < 0) {
 #else
     unsigned sndopt;
+    if (getsockopt(sd, SOL_SOCKET, SO_SNDBUF, &sndopt, sizeof(sndopt)) < 0) {
+        wbloglf("getsockopt SO_SNDBUF error:%d", errno);
+    }
     unsigned rcvopt;
-    getsockopt(sd, SOL_SOCKET, SO_SNDBUF, &sndopt, sizeof(sndopt));
-    getsockopt(sd, SOL_SOCKET, SO_RCVBUF, &rcvopt, sizeof(rcvopt));
+    if (getsockopt(sd, SOL_SOCKET, SO_RCVBUF, &rcvopt, sizeof(rcvopt)) < 0) {
+        wbloglf("getsockopt SO_RCVBUF error:%d", errno);
+    }
     wblogf("sndbuf:%ld, rcv:%ld", sndopt, rcvopt);
     if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
 #endif
